e595fe82139ebb83a51e3c8b0ac5d87e1ca263f8
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2019-01-09 21:23:13 +0800

    Add files via upload

---
+++ takewayking/src/java/takeaway/tag/BoxTag.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takeaway.tag;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+/**
+ *
+ * @author user
+ */
+public class BoxTag extends SimpleTagSupport {
+
+    private String name, category, address, tel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        try {
+            PageContext pageContext = (PageContext) getJspContext();
+            JspWriter out = pageContext.getOut();
+            out.println("<div class=\"contentbox\">\n"
+                    + "         <a href=\"#\" title=\"" + name + "\n\nCategory: " + category
+                    + "\n\nAddress: " + address + "\n\nTel: " + tel + "\" rel=\"bookmark\" class=\"thumb-post\">\n"
+                    + "             <img src=\"./Menu/3.jpg\" alt=\"omk\" height=\"80\" width=\"80\">\n"
+                    + "         </a>\n"
+                    + "     </div>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
